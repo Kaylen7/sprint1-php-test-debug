ef8fd054f423dceb7577908f33f7bf666f48a440
Sara DM <ochuck.sdm@gmail.com>
2024-09-25 18:19:42 +0200

    fixed type conversion bug  🫠

--- src/nivell1/exercici2/getNota.php
+++ src/nivell1/exercici2/getNota.php
@@ -6,11 +6,12 @@ function getNota(float $nota): string{
     if($nota > 100 || $nota < 0){
         return "Nota fora dels límits.";
     }
+
     if($nota < 10){
         $nota *= 10;
     }
     
-    switch($nota){
+    switch(true){
         case ($nota >= 60):
             return "Primera Divisió";
         case ($nota >= 45):
